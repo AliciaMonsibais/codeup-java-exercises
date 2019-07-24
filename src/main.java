90a95dbb31c3c4c8198eb1d5e1966f1996231065
AliciaMonsibais <alicia.monsibais1@gmail.com>
2019-07-24 16:36:48 -0500

    updating with proper files

---
+++ src/main.java
@@ -0,0 +1,40 @@
+public class main {
+    public static void main(String[] args) {
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "yarn";
+        System.out.println(myString);
+
+        double myNumber= 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+
+        int x = 4;
+        x += 5;
+        System.out.println(x);
+
+        int z = 3;
+        int y = 4;
+        y *= z;
+        System.out.println(y);
+
+        int a = 10;
+        int b = 2;
+        a /= b;
+        b -= a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
