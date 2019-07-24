d289a66a8d82339c437201f340913b0a98d6e107
AliciaMonsibais <alicia.monsibais1@gmail.com>
2019-07-24 16:22:50 -0500

    adding java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,46 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
+        int myFavoriteNumber;
+        String myString;
+        double myNumber;
+//        class things;
+
+        myFavoriteNumber = 13;
+        myString = "yarn";
+        myNumber = 3.14;
+
+        System.out.println(myFavoriteNumber);
+        System.out.println(myString);
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+
+        int x = 4;
+        x += 5;
+        System.out.println(x);
+
+        int z = 3;
+        int y = 4;
+        y *= z;
+        System.out.println(y);
+
+        int a = 10;
+        int b = 2;
+        a /= b;
+        b -= a;
+        System.out.println(a);
+        System.out.println(b);
     }
 }
+
