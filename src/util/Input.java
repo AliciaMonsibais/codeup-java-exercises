712d533760cfd9e25ddc7780f9f930fb8115c76a
AliciaMonsibais <alicia.monsibais1@gmail.com>
2019-08-21 20:23:15 -0500

    clean up

--- src/util/Input.java
+++ src/util/Input.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Input {
 
-    private Scanner scanner = new Scanner(System.in);
+        private Scanner scanner = new Scanner(System.in);
 
     public String getString(String msg){
         System.out.println(msg);
